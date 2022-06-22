52e3151ab85550ee8d3b76a26a1103184727db89
Misqke <misqke.rust@yahoo.com>
2022-06-22 16:54:56 -0400

    work on documentation

--- controllers/randomControllers.js
+++ controllers/randomControllers.js
@@ -26,42 +26,17 @@ const randomPokemon = (req, res) => {
 
 const whosThatPokemon = (req, res) => {
   const limit = Number(req.query.limit) || 15;
-  const minNum = Number(req.query.minNum) || 1;
-  const maxNum = Number(req.query.maxNum) || 898;
+  const minNum = Number(req.query.min) || 1;
+  const maxNum = Number(req.query.max) || 898;
   try {
     const pokemonList = pokemon.slice(minNum - 1, maxNum);
+    const names = pokemonList.map((pokemon) => pokemon.name);
     const randomPokemon = [];
     while (randomPokemon.length < limit) {
-      const randomNumbers = [];
-      while (randomNumbers.length < 4) {
-        const randIndex = Math.floor(Math.random() * pokemonList.length);
-        if (randomNumbers.length === 0) {
-          const usedPokemon = randomPokemon.map(
-            (question) => question.answer.name
-          );
-          if (!usedPokemon.includes(pokemonList[randIndex].name)) {
-            randomNumbers.push(randIndex);
-          }
-        } else if (!randomNumbers.includes(randIndex)) {
-          randomNumbers.push(randIndex);
-        }
-      }
-      const answer = {
-        name: pokemonList[randomNumbers[0]].name,
-        desc: pokemonList[randomNumbers[0]].desc1,
-        img: pokemonList[randomNumbers[0]].img,
-      };
-      const question = {
-        answer,
-        choices: [
-          pokemonList[randomNumbers[1]].name,
-          pokemonList[randomNumbers[2]].name,
-          pokemonList[randomNumbers[3]].name,
-        ],
-      };
-      randomPokemon.push(question);
+      const randomIndex = Math.floor(Math.random() * pokemonList.length);
+      randomPokemon.push(pokemonList.splice(randomIndex, 1)[0]);
     }
-    res.status(200).json({ pokemon: randomPokemon });
+    res.status(200).json({ pokemon: randomPokemon, names });
   } catch (error) {
     console.log(error);
     res.json(error);
